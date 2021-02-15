f8fc1d3b86c70a8176a6bd4c73c3f0acc0061988
Shouvick Halder <deeprony7@gmail.com>
2021-02-15 23:02:16 +0530

    added vpc resource and igw

--- Provision Instance in a VPC/main.tf
+++ Provision Instance in a VPC/main.tf
@@ -4,3 +4,18 @@ provider "aws" {
   secret_key = ""
 }
 
+# 1. Create vpc
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+
+# 2. Create Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+}
