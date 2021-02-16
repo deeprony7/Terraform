d73f57e70d756a5e31eb03e659e7273e2954b5fa
Shouvick Halder <deeprony7@gmail.com>
2021-02-16 14:00:34 +0530

    added route table and subnet

--- Provision Instance in a VPC/main.tf
+++ Provision Instance in a VPC/main.tf
@@ -19,3 +19,36 @@ resource "aws_vpc" "prod-vpc" {
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.prod-vpc.id
 }
+
+# 3. Create Custom Route Table
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Prod"
+  }
+}
+
+# 4. Create a Subnet
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id = aws_vpc.prod-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
